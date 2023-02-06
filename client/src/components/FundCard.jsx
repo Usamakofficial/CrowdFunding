717e00797931a5ae49f2d874d2ef42e637f39219
Usamakofficial <119401471+sabifi721@users.noreply.github.com>
2023-02-06 12:39:36 +0500

    changes

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -7,10 +7,10 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
   const remainingDays = daysLeft(deadline);
   
   return (
-    <div className="sm:w-[288px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer" onClick={handleClick}>
+    <div className="upper_sec sm:w-[288px] w-full border-amber-700 rounded-[15px] cursor-pointer" onClick={handleClick}>
       <img src={image} alt="fund" className="w-full h-[158px] object-cover rounded-[15px]"/>
 
-      <div className="flex flex-col p-4">
+      <div className="nft-image flex flex-col p-4">
         <div className="flex flex-row items-center mb-[18px]">
           <img src={tagType} alt="tag" className="w-[17px] h-[17px] object-contain"/>
           <p className="ml-[12px] mt-[2px] font-epilogue font-medium text-[12px] text-[#808191]">Education</p>
@@ -21,7 +21,7 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
           <p className="mt-[5px] font-epilogue font-normal text-[#808191] text-left leading-[18px] truncate">{description}</p>
         </div>
 
-        <div className="flex justify-between flex-wrap mt-[15px] gap-2">
+        <div className="lower_sec flex justify-between flex-wrap mt-[15px] gap-2">
           <div className="flex flex-col">
             <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]">{amountCollected}</h4>
             <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate">Raised of {target}</p>
